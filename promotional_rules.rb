0a8e62eb857c7a9216a141b67fb50f261a50e158
iferunsewe <iferunsewe@gmail.com>
2018-06-16 13:47:36 +0100

    error raised if no items are passed to the promotional rules

--- promotional_rules.rb
+++ promotional_rules.rb
@@ -1,3 +1,13 @@
 class PromotionalRules
+  def initialize(items:)
+    raise ArgumentError, "All items must be an Item" unless contains_items?(items)
+
+  end
+
+  private
+
+  def contains_items?(items)
+    !items.empty? && items.all?{|item| item.is_a? Item}
+  end
 
 end
--- spec/promotional_rules_spec.rb
+++ spec/promotional_rules_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe PromotionalRules do
+  subject(:promotional_rules) { described_class.new(items: items) }
+  let(:items) { [] }
 
+  it 'must be given items' do
+    expect { promotional_rules }.
+        to raise_error(ArgumentError, 'All items must be an Item')
+  end
 end
