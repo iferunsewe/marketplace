2e9ad9a7ba1c8c956281d92805fb286bb3129738
iferunsewe <iferunsewe@gmail.com>
2018-06-17 13:18:38 +0100

    implementation and testing for total

--- checkout.rb
+++ checkout.rb
@@ -18,4 +18,22 @@ class Checkout
       @basket << item_code
     end
   end
+
+  def total
+    return 0 if basket.empty?
+    apply_discounts
+  end
+
+  private
+
+  def apply_discounts
+    original_total = calculate_original_total
+    @promotional_rules.apply_discounts(original_total, basket)
+  end
+
+  def calculate_original_total
+    @basket.map{|item_code|
+      ITEMS.detect{|item| item[:code] == item_code }[:price]
+    }.reduce(0, :+)
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe Checkout do
-  subject(:checkout){ Checkout.new(promotional_rules) }
-  let(:promotional_rules){
-    module DummyPromotionalRules
-      def apply_fifty_percent_discount(total)
-        if total > 1000
-          total = total - (total / 50)
-        end
-        total
-      end
+  subject(:checkout){ Checkout.new(DummyPromotionalRules) }
+  module DummyPromotionalRules
+    extend self
+    def apply_discounts(total, _)
+      apply_fifty_percent_discount(total)
     end
-  }
+
+    def apply_fifty_percent_discount(total)
+      if total > 1000
+        total = total / 2
+      end
+      total
+    end
+  end
 
   it 'has an empty basket' do
     expect(checkout.basket).to be_empty
@@ -37,4 +40,37 @@ RSpec.describe Checkout do
     end
   end
 
+  describe '.total' do
+    subject(:total){ checkout.total }
+
+    it 'returns 0 when the basket is empty' do
+      expect(total).to eq(0)
+    end
+
+    context 'when the basket has items and discounts are applied' do
+      it 'returns a discounted total' do
+        scan_item('001')
+        scan_item('002')
+        discounted_total = (get_item_price('001') + get_item_price('002')) / 2
+        expect(total).to eq(discounted_total)
+      end
+
+
+    end
+
+    context 'when the basket items and discounts are not applied' do
+      it 'returns a total with no discounts' do
+        scan_item('001')
+        expect(total).to eq(get_item_price('001'))
+      end
+    end
+
+    def get_item_price(item_code)
+      Checkout::ITEMS.detect{|item| item[:code] == item_code}[:price]
+    end
+
+    def scan_item(item_code)
+      checkout.scan(item_code)
+    end
+  end
 end
